3a46413d5bc82dfd6794ee19a52deddf5f413fb2
Egor Makarenko <egormkn@yandex.ru>
2021-10-14 22:37:34 +0300

    Add compression middleware for NestJS
    Command:
    `npm i --save compression && npm i --save-dev @types/compression`

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,12 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(compression());
 
   app.useGlobalPipes(
     new ValidationPipe({
