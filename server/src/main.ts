4709f51b0656e00fd649e27b515e482f49d85296
Egor Makarenko <egormkn@yandex.ru>
2021-10-14 22:36:00 +0300

    Add validation pipe for NestJS
    Command:
    `npm i --save class-validator class-transformer`

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,24 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      validateCustomDecorators: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      forbidUnknownValues: true,
+      exceptionFactory: (validationErrors = []) => {
+        const errors = this.flattenValidationErrors(validationErrors);
+        return new BadRequestException(errors);
+      },
+    }),
+  );
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 80);
