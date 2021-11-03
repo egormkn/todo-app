996279d901d1bc79dfed880737dec454b8662024
Egor Makarenko <egormkn@yandex.ru>
2021-11-03 19:29:49 +0300

    Fix localStorage for Angular Universal

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { JwtModule } from '@auth0/angular-jwt';
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LOCAL_STORAGE } from '@ng-web-apis/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { IndexComponent } from './index/index.component';
+
+export function jwtOptionsFactory(localStorage: Storage) {
+  return {
+    tokenGetter: () => localStorage.getItem('access_token'),
+    skipWhenExpired: false,
+  };
+}
+
+const jwtOptionsProvider = {
+  provide: JWT_OPTIONS,
+  useFactory: jwtOptionsFactory,
+  deps: [LOCAL_STORAGE],
+};
 
 @NgModule({
   // eslint-disable-next-line prettier/prettier
@@ -21,12 +35,7 @@ import { IndexComponent } from './index/index.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: (req) => localStorage?.getItem('access_token'),
-        skipWhenExpired: false,
-      },
-    }),
+    JwtModule.forRoot({ jwtOptionsProvider }),
     FontAwesomeModule,
     NgbModule,
     AppRoutingModule,
