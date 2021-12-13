b2c800861874c6b80a06a487acaa9d9cf797030f
Egor Makarenko <egormkn@yandex.ru>
2021-12-14 00:02:04 +0300

    Add tests for authentication

--- server/src/auth/auth.controller.spec.ts
+++ server/src/auth/auth.controller.spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserInterface } from '../common/interfaces/user.interface';
 import { createMockObj } from '../common/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 describe('AuthController', () => {
   let authController: AuthController;
   let authService: jest.Mocked<AuthService>;
 
   beforeEach(async () => {
-    const mockedAuthService = createMockObj<AuthService>(['logIn', 'signUp']);
+    const mockedAuthService = createMockObj<AuthService>(['logIn', 'signUp', 'issueJwt', 'logOut']);
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
@@ -19,8 +21,68 @@ describe('AuthController', () => {
     authService = module.get(AuthService) as jest.Mocked<AuthService>;
   });
 
-  it('should be defined', () => {
-    expect(authController).toBeDefined();
-    expect(authService).toBeDefined();
+  describe('#logIn', () => {
+    it('should return JWT', async () => {
+      const user = {} as UserInterface;
+      const token = 'test_token';
+      authService.issueJwt.mockResolvedValue(token);
+
+      const result = await authController.logIn(user);
+
+      expect(result).toEqual({ access_token: token });
+      expect(authService.issueJwt).toBeCalledWith(user);
+    });
+  });
+
+  describe('#signUp', () => {
+    it('should create user and return JWT', async () => {
+      const dto = {} as SignUpDto;
+      const user = {} as UserInterface;
+      const token = 'test_token';
+      authService.signUp.mockResolvedValue(user);
+      authService.issueJwt.mockResolvedValue(token);
+
+      const result = await authController.signUp(dto);
+
+      expect(result).toEqual({ access_token: token });
+      expect(authService.signUp).toBeCalledWith(dto);
+      expect(authService.issueJwt).toBeCalledWith(user);
+    });
+  });
+
+  describe('#logOut', () => {
+    it('should call AuthService#logOut', async () => {
+      const user = {} as UserInterface;
+
+      await authController.logOut(user);
+
+      expect(authService.logOut).toBeCalledWith(user);
+    });
+  });
+
+  describe('#logInWithGoogle', () => {
+    it('should return JWT', async () => {
+      const user = {} as UserInterface;
+      const token = 'test_token';
+      authService.issueJwt.mockResolvedValue(token);
+
+      const result = await authController.logInWithGoogle(user);
+
+      expect(result).toEqual({ access_token: token });
+      expect(authService.issueJwt).toBeCalledWith(user);
+    });
+  });
+
+  describe('#logInWithVkontakte', () => {
+    it('should return JWT', async () => {
+      const user = {} as UserInterface;
+      const token = 'test_token';
+      authService.issueJwt.mockResolvedValue(token);
+
+      const result = await authController.logInWithVkontakte(user);
+
+      expect(result).toEqual({ access_token: token });
+      expect(authService.issueJwt).toBeCalledWith(user);
+    });
   });
 });
--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -1,9 +1,16 @@
+import { BadRequestException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
+import { UserPasswordInterface } from '../common/interfaces/user-password.interface';
 import { createMockObj } from '../common/testing';
+import { omit } from '../common/utilities';
+import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { LogInDto } from './dto/log-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -12,9 +19,9 @@ describe('AuthService', () => {
   let usersService: jest.Mocked<UsersService>;
 
   beforeEach(async () => {
-    const mockedConfigService = createMockObj<ConfigService>();
+    const mockedConfigService = createMockObj<ConfigService>(['get']);
     const mockedJwtService = createMockObj<JwtService>();
-    const mockedUsersService = createMockObj<UsersService>();
+    const mockedUsersService = createMockObj<UsersService>(['findUserByUsername', 'createUser']);
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -31,10 +38,90 @@ describe('AuthService', () => {
     usersService = module.get(UsersService) as jest.Mocked<UsersService>;
   });
 
-  it('should be defined', () => {
-    expect(authService).toBeDefined();
-    expect(configService).toBeDefined();
-    expect(jwtService).toBeDefined();
-    expect(usersService).toBeDefined();
+  describe('#logIn', () => {
+    it('should return user if passwords match', async () => {
+      const username = 'username';
+      const password = 'password';
+      const logInDto = { username, password } as LogInDto;
+      const hash = await bcrypt.hash(password, 10);
+      const user = { username, password: hash } as UserPasswordInterface;
+      usersService.findUserByUsername.mockResolvedValue(user as User);
+
+      const result = await authService.logIn(logInDto);
+
+      expect(result).toEqual(omit(user, 'password'));
+      expect(result).not.toHaveProperty('password');
+    });
+
+    it('should throw an exception if user was not found', async () => {
+      const username = 'username';
+      const password = 'password';
+      const logInDto = { username, password } as LogInDto;
+      usersService.findUserByUsername.mockImplementation(() => {
+        throw new NotFoundException(`User "${username}" was not found`);
+      });
+
+      try {
+        const _result = await authService.logIn(logInDto);
+        fail('exception expected');
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+    it('should throw an exception if passwords do not match', async () => {
+      const username = 'username';
+      const password = 'wrong-password';
+      const logInDto = { username, password } as LogInDto;
+      const hash = await bcrypt.hash('password', 10);
+      const user = { username, password: hash } as UserPasswordInterface;
+      usersService.findUserByUsername.mockResolvedValue(user as User);
+
+      try {
+        const _result = await authService.logIn(logInDto);
+        fail('exception expected');
+      } catch (e) {
+        expect(e).toBeInstanceOf(UnauthorizedException);
+      }
+    });
+  });
+
+  describe('#signUp', () => {
+    it('should return the user that was created', async () => {
+      const username = 'username';
+      const password = 'password';
+      const signUpDto = { username, password } as SignUpDto;
+      const saltRounds = 10;
+      configService.get.mockReturnValue(saltRounds);
+      const hash = await bcrypt.hash(password, saltRounds);
+      const user = { username, password: hash } as UserPasswordInterface;
+      usersService.createUser.mockResolvedValue(user as User);
+
+      const result = await authService.signUp(signUpDto);
+
+      expect(result).toEqual(omit(user, 'password'));
+      expect(result).not.toHaveProperty('password');
+      expect(configService.get).toBeCalled();
+      expect(usersService.createUser).toBeCalled();
+    });
+
+    it('should throw an exception if user was not created', async () => {
+      const username = 'username';
+      const password = 'password';
+      const signUpDto = { username, password } as SignUpDto;
+      const saltRounds = 10;
+      configService.get.mockReturnValue(saltRounds);
+      usersService.createUser.mockImplementation(() => {
+        throw new BadRequestException();
+      });
+
+      try {
+        const _result = await authService.signUp(signUpDto);
+        fail('exception expected');
+      } catch (e) {
+        expect(usersService.createUser).toBeCalled();
+        expect(e).toBeInstanceOf(BadRequestException);
+      }
+    });
   });
 });
